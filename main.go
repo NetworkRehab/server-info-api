b2bad962bed0e9a1a2831b8b9356880de1b0629a
Nick Donathan <31076657+ndonathan@users.noreply.github.com>
2024-12-16 20:51:13 -0600

    changed port to 8081

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	log.Println("Starting server on :8080")
+	log.Println("Starting server on :8081")
 	http.HandleFunc("/", handler)
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
